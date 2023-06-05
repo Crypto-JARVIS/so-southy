3be5e4957a200c141e2936a518c5f941e19eb835
AkshayGupt <akshay.189301019@muj.manipal.edu>
2023-06-06 00:51:51 +0530

    :tada: Initial admin setup

---
+++ src/Components/Admin/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//This component will cover the admin login
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
---
+++ src/Components/Admin/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//This component will cover the admin login
+const Login = () => {
+  return (
+    <div>Login Page</div>
+  )
+}
+
+export default Login
