3708a218fe31147e02484247543b9b7230361635
Akash <kingakashgupta456@gmail.com>
2023-09-18 16:53:37 +0530

    :sparkles: Style changes in Brand Image

--- src/Components/Home/Brand.jsx
+++ src/Components/Home/Brand.jsx
@@ -4,21 +4,28 @@ import bgimg from "../../Images/SOSOUTHY.jpg";
 
 const Brand = () => {
   return (
-    <div className={cstyles.brand}>
-        <div className={cstyles.brandDesc}>
+    <>
+      <div className={cstyles.vsize}>
+        <div className={cstyles.brand}>
+          <div className={cstyles.brandDesc}>
             <div>
-                <h2>STRAIGHT FROM SOUTH <br/>TO MOUTH</h2>
+              <h2>
+                STRAIGHT FROM SOUTH <br />
+                TO MOUTH
+              </h2>
             </div>
             <div className={cstyles.brandOC}>
-                MONDAY thru FRIDAY 5pm – 11pm
-                <br/>
-                SATURDAY and SUNDAY 10am – 11pm 
+              MONDAY thru FRIDAY 5pm – 11pm
+              <br />
+              SATURDAY and SUNDAY 10am – 11pm
             </div>
+          </div>
+          <div className={cstyles.brandName}>
+            <img src={bgimg} alt="Brand"/>
+          </div>
         </div>
-        <div className={cstyles.brandName}>
-            <img src={bgimg} />
-        </div>
-    </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/Components/Home/Founder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import cstyles from "./Founder.module.css";
+
+const Founder = () => {
+  return (
+    <div>
+      <h2>FOUNDER</h2>
+    </div>
+  );
+};
+
+export default Founder;
